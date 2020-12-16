fca4145fb0de7f3a96fb3b9f3f148a4e0365db48
iREQ <contact@irequire.dev>
2020-12-16 10:48:57 +0100

    Added Import/Export

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueClipboard from 'vue-clipboard2'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import './darktheme.min.css'
 
 Vue.config.productionTip = false
 
+VueClipboard.config.autoSetContainer = true
+Vue.use(VueClipboard)
 Vue.use(Buefy, {
   defaultIconPack: 'fas'
 })
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -13,6 +13,54 @@
             {{ feed.url }}
           </b-tag>
         </div>
+        <div class="container mt-5">
+          <div class="columns">
+            <div class="column">
+              <b-button type="is-primary" class="mr-2" icon-left="file-import" @click="openImportModal">Import Feeds</b-button>
+              <b-button type="is-primary" icon-left="file-export" @click="openExportModal">Export Feeds</b-button>
+            </div>
+          </div>
+        </div>
+        <b-modal v-model="exportModalActive" :width="640">
+          <div class="card">
+            <div class="card-content">
+              <div class="content">
+                <h1>Export</h1>
+                <b-field>
+                  <b-input placeholder="Feed JSON"
+                           v-model="feedsJSON"
+                           type="text"
+                           icon="rss"
+                           readonly>
+                  </b-input>
+                </b-field>
+                <hr>
+                <b-button type="is-primary" icon-left="copy" @click="copyFeed">Copy</b-button>
+              </div>
+            </div>
+          </div>
+        </b-modal>
+        <b-modal v-model="importModalActive" :width="640">
+          <div class="card">
+            <div class="card-content">
+              <div class="content">
+                <h1>Import</h1>
+                <b-field>
+                  <b-input placeholder="Feed JSON"
+                           v-model="feedsJSON"
+                           type="text"
+                           icon="rss">
+                  </b-input>
+                </b-field>
+                <hr>
+                <b-message v-show="importMessage.length > 0" :type="importMessageType" :closable="false">
+                  {{ importMessage }}
+                </b-message>
+                <b-button :disabled="feedsJSON.length === 0" type="is-primary" icon-left="file-import" :loading="processingImport" @click="doImport">Import</b-button>
+              </div>
+            </div>
+          </div>
+        </b-modal>
       </b-field>
     </div>
   </div>
@@ -27,6 +75,12 @@ export default Vue.extend({
   data: () => ({
     addFeedURL: "",
     feeds: [] as SavedFeed[],
+    exportModalActive: false,
+    importModalActive: false,
+    feedsJSON: "",
+    processingImport: false,
+    importMessage: "",
+    importMessageType: ""
   }),
   created() {
     this.feeds = JSON.parse(localStorage.getItem('feeds') ?? '[]');
@@ -47,6 +101,46 @@ export default Vue.extend({
       const index = this.feeds.findIndex((value: SavedFeed) => value.url === url);
       this.feeds.splice(index, 1);
       localStorage.setItem('feeds', JSON.stringify(this.feeds));
+    },
+    openExportModal(){
+      this.feedsJSON = JSON.stringify(this.feeds);
+      this.exportModalActive = true;
+    },
+    copyFeed(){
+      this.$copyText(this.feedsJSON);
+    },
+    openImportModal(){
+      this.feedsJSON = "";
+      this.importModalActive = true;
+    },
+    doImport(){
+      this.processingImport = true;
+      this.importMessage = "";
+      try {
+        const parsedJson = JSON.parse(this.feedsJSON);
+        if(parsedJson instanceof Array){
+          for(const item of parsedJson){
+            // eslint-disable-next-line no-prototype-builtins
+            if (item.hasOwnProperty('id') && item.hasOwnProperty('url')) {
+              this.feeds = parsedJson;
+              localStorage.setItem('feeds', JSON.stringify(this.feeds));
+              this.importModalActive = false;
+            }else{
+              this.importMessageType = "is-danger";
+              this.importMessage = "Error: Couldn't parse JSON.";
+            }
+          }
+        }else{
+          this.importMessageType = "is-danger";
+          this.importMessage = "Error: Couldn't parse JSON.";
+        }
+      }catch(e){
+        if(e instanceof SyntaxError ) {
+          this.importMessageType = "is-danger";
+          this.importMessage = "Error: " + e;
+        }
+      }
+      this.processingImport = false;
     }
   }
 });
